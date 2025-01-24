ad1f41b28b782d5bf678a942a7f8d1bd85d3fdd3
damshizzzz <amgoronos22@src.pshs.edu.ph>
2025-01-24 23:27:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let pi; 
+let radius; 
+let circumference;
+let surfaceArea;
+let volume;
+
+pi = 3.14; 
+radius = 6.378e6; 
+circumference = 2 * pi * radius;
+surfaceArea = 4 * pi * (radius**2);
+volume = (4 / 3) * pi * (radius**3);
+   
+   document.getElementById("radius").innerHTML = radius;
+   document.getElementById("circumference").innerHTML = circumference;
+   document.getElementById("surfaceArea").innerHTML = surfaceArea;
+   document.getElementById("volume").innerHTML = volume;
